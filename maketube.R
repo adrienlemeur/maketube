b0f3336a93971aa1da6d389e9dab7a5bc3c91746
Adrien Le Meur <71882657+adrienlemeur@users.noreply.github.com>
2025-03-10 15:59:02 +0100

    Add files via upload

--- maketube.R
+++ maketube.R
@@ -44,7 +44,7 @@
                 help="the parameters of the GTR model"),
     make_option(c("--indel_scaling_factor"), type = "character", default=as.numeric(5),
                 help="indel/snp scaling factor"),
-    make_option(c("--slope"), type='numeric', default=300,
+    make_option(c("--slope"), type='character', default="50,300",
                 help="Size of the slope between two structural variants"),
     make_option(c("--threads"), type='numeric', default=4,
                 help="Number of threads used for fastq generation"),
@@ -54,7 +54,7 @@
   
   opt_parser = OptionParser(option_list=option_list);
   opt = parse_args(opt_parser);
-  
+
   text2numeric <- function(x) {eval(parse(text=as.character(x)))}
   
   opt$mutation_rate = text2numeric(opt$mutation_rate)
@@ -63,6 +63,7 @@
   
   opt$TCAGI <- sapply(strsplit(opt$TCAGI, ",")[[1]], text2numeric, USE.NAMES = F)
   opt$ABCDEF <- sapply(strsplit(opt$ABCDEF, ",")[[1]], text2numeric, USE.NAMES = F)
+  opt$slope <- sapply(strsplit(opt$slope, ",")[[1]], text2numeric, USE.NAMES = F)
   
   if (is.null(opt$reference)){
     print_help(opt_parser)
@@ -85,8 +86,8 @@ if(debug){
   opt$duplication_region_size = 10000
   opt$haplotype_count = 10
   opt$pop_count = 2
-  opt$structural_variants = 3
-  opt$deletion_count = 3
+  opt$structural_variants = 1
+  opt$deletion_count = 10
   opt$unmuted = F
   opt$prefix = "no_prefix"
   opt$mutation_rate = 1.23e-7
@@ -94,11 +95,10 @@ if(debug){
   opt$TCAGI = c(0.172, 0.329, 0.172, 0.329, 0)
   opt$ABCDEF = c(0.65, 0.05, 0.21, 0.27, 0.02, 0.65)
   opt$indel_scaling_factor = 0.125
-  opt$slope = 250
+  opt$slope = c(10, 250)
   opt$threads = 1
   opt$output = "debug"
 }
-
 
 {
   reference_sequence <- list()
@@ -110,9 +110,9 @@ if(debug){
   #names == original position != actual nucleotide position
   names(reference_sequence$sequence) <- as.character(1:length(reference_sequence$sequence))
   chromosome_name <- unlist(strsplit(names(reference_sequence.seqinr), '\t'))[1]
-  
+
   transposons_loci <- apply(read.table(opt$transposon, header = F, sep = "\t", col.names = c("chrom", "start", "stop")), 1, function(x) as.list(sapply(x, trimws) ))
-  
+
   one_big_pool <- readLines(opt$nonhomoseq_pool)
   kmer_size <- length(strsplit(one_big_pool[1], "")[[1]])
   
@@ -133,15 +133,18 @@ for(SV_set in 1:opt$structural_variants){
   
   cat(paste0("Starting transposition process for ", SV_name, "\n"))
   dir.create(SV_folder_path, recursive = T, showWarnings = F)
+  
   {
     #setting a vector of position that can be picked out
     #F = free //T = either used by a deletion region or a transposon jump
     neo_sequence <- list() ; neo_sequence$sequence <- reference_sequence$sequence
     range_to_pick <- rep(0, length(neo_sequence$sequence))
-    
+    range_to_pick[1:max(opt$slope)] <- 1
+    range_to_pick[seq( length(range_to_pick)-max(opt$slope), length(range_to_pick))] <- 1
+
     #setting the initial position of transposons as unpickable
     for(i in 1:length(transposons_loci)){range_to_pick[seq(transposons_loci[[i]]$start, transposons_loci[[i]]$stop)] <- 1}
-    
+  
     #setting the post-jump transposon positions
     #object transposon jump is a list with :
     #transposon_jumps.partition[[index]]$start // transposon_jumps.partition[[index]]$stop => initial position
@@ -151,16 +154,15 @@ for(SV_set in 1:opt$structural_variants){
       transposon_jumps.partition <- list()
       for(i in 1:length(transposons_loci)){
         #until post-jump position does not overlap with an unpickable nucleotide, retry
-        #does the trick for a low number of mutation
+        #does the trick for a low number of variations
         repeat{
-          insertion_new_start <- sample(1:length(neo_sequence$sequence), 1)
+          insertion_new_start <- sample(max(opt$slope):length(neo_sequence$sequence)-max(opt$slope), 1)
           insertion_new_stop <- insertion_new_start + abs(transposons_loci[[i]]$stop - transposons_loci[[i]]$start)
-          insertion_sites <- seq(insertion_new_start, insertion_new_stop)
-          slopped_insertion_site <- seq(insertion_new_start - 300, insertion_new_stop + 300)
+          #insertion_sites <- seq(insertion_new_start, insertion_new_stop)
+          slopped_insertion_site <- seq(insertion_new_start - max(opt$slope), insertion_new_start + max(opt$slope))
           #do{pick a transposon} while{transposon new positions overlaps with unpickable positions}
           if(sum(range_to_pick[slopped_insertion_site] == 1) == 0) {break}
         }
-        
         #when done, registers the jump
         transposon_jumps.partition[[i]] <- transposons_loci[[i]]
         transposon_jumps.partition[[i]]$stop <- transposons_loci[[i]]$stop
@@ -175,10 +177,10 @@ for(SV_set in 1:opt$structural_variants){
       for(i in seq(1, deletion_count)){
         #until deletions.partition are completely independant, pick another (position and size)
         repeat{
-          deletion <- sample(1:length(neo_sequence$sequence), size = 1)
+          deletion <- sample(max(opt$slope):length(neo_sequence$sequence)-max(opt$slope), size = 1)
           deletion_size <- floor(rgamma(1, shape = 1, scale = 3500))
-          deleted_nucleotides <- seq(deletion, deletion + deletion_size)
-          slopped_deletion_site <- seq(deletion - opt$slope, deletion + deletion_size + opt$slope)
+          #deleted_nucleotides <- seq(deletion, deletion + deletion_size)
+          slopped_deletion_site <- seq(deletion - max(opt$slope), deletion + deletion_size + max(opt$slope))
           if(all(range_to_pick[slopped_deletion_site] == 0)) {break}
         }
         #remove positions of the deletion from the positions to pick from so it does not overlap
@@ -195,10 +197,9 @@ for(SV_set in 1:opt$structural_variants){
       
       #this one works a bit differently
       duplication_region_window_start <- sample(1:(length(range_to_pick)-opt$duplication_region_size), size = 1)
-      range_to_pick[duplication_region_window_start:duplication_region_window_start+35000] = 1
-      
+      range_to_pick[duplication_region_window_start:duplication_region_window_start+opt$duplication_region_size] = 1
+
       nohomoseq.list = list(info = list(cumulative_size = 0, wanted_size = sample(4400000*seq(0.002, 0.008, by = 0.00001), 1)))
-      
       i = 1
       while(nohomoseq.list[["info"]]$cumulative_size <= nohomoseq.list[["info"]]$wanted_size){
         new_sequence_size <- floor(rgamma(1, shape = 1, scale = 3500))
@@ -220,7 +221,7 @@ for(SV_set in 1:opt$structural_variants){
       nohomoseq.partition <- list()
       for(non_hologuous_index in names(tail(nohomoseq.list, -1))){
         insertion_site <- sample(which(range_to_pick == 0), 1)
-        slopped_insertion_site <- seq(insertion_site - opt$slope, insertion_site + opt$slope)
+        slopped_insertion_site <- seq(insertion_site - max(opt$slope), insertion_site + max(opt$slope))
         
         nohomoseq.partition[[non_hologuous_index]] <- list()
         nohomoseq.partition[[non_hologuous_index]]$chrom <- chromosome_name
@@ -232,12 +233,11 @@ for(SV_set in 1:opt$structural_variants){
         nohomoseq.partition[[non_hologuous_index]]$class = "insertion"
       }
     } #END OF PARTITION CREATION
-    
-    
+
     {
       #insertion of non homologuous sequences	
       for(insertion_nohomoseq in nohomoseq.partition){
-        insertion_site <- as.numeric(max(which(names(neo_sequence$sequence) %in% insertion_nohomoseq$start)))
+        insertion_site <- as.numeric(which(names(neo_sequence$sequence) %in% insertion_nohomoseq$start))
         neo_sequence$sequence = c(neo_sequence$sequence[1:insertion_site-1], insertion_nohomoseq$sequence, neo_sequence$sequence[insertion_site:length(neo_sequence$sequence)])
       }
 
@@ -253,41 +253,45 @@ for(SV_set in 1:opt$structural_variants){
       transposon_sequence <- NULL
       for(transpo_jump in transposon_jumps.partition){
         #min to selecte the position in the non duplicated area
-        new_start_position <- min(which(names(neo_sequence$sequence) == transpo_jump$new_start))
+        new_start_position <- which(names(neo_sequence$sequence) == transpo_jump$new_start)
         transposon_sequence <- reference_sequence$sequence[seq(min(transpo_jump$start, transpo_jump$stop), max(transpo_jump$start, transpo_jump$stop))]
         neo_sequence$sequence <- c(neo_sequence$sequence[1:new_start_position-1], transposon_sequence, neo_sequence$sequence[new_start_position:length(neo_sequence$sequence)])
       }
 
       #duplication region 
       neo_sequence$sequence <- c(neo_sequence$sequence, reference_sequence$sequence[seq(duplication_region_window_start, duplication_region_window_start+opt$duplication_region_size)])
-      
     } #PLAYING THE PARTITION
     
     {
       transposons.info <- sapply(transposon_jumps.partition, function(x) {
-        previous_start <- min(as.numeric(which(names(neo_sequence$sequence) == x$start - 1)))
         pos_transpo_start <- min(as.numeric(which(names(neo_sequence$sequence) == x$new_start)))
-        return(list(
-          c(chromosome_name, previous_start - opt$slope, previous_start + opt$slope, "IS_scar_site"),
-          c(chromosome_name, pos_transpo_start - opt$slope, pos_transpo_start + opt$slope, "IS_flanking_region"))
-        )
+        lapply(opt$slope, function(one_slope){
+          list(c(chromosome_name, x$start - one_slope, x$start, "IS_scar_regions", one_slope), c(chromosome_name, x$stop, x$stop + one_slope, "IS_scar_regions", one_slope), c(chromosome_name, pos_transpo_start - one_slope, pos_transpo_start + one_slope, "IS_flanking_regions", one_slope))
+        })
       })
-      transposons.info <- do.call(rbind, transposons.info)
-      
-      deletions.info <- as.data.frame(do.call(rbind, deletions.partition))
-      deletions.info <- cbind(deletions.info$chrom, as.numeric(deletions.info$new_start) - opt$slope, as.numeric(deletions.info$new_start) + opt$slope, "DR_scar_site")
-      
-      duplication.info <- rbind(c(chromosome_name, duplication_region_window_start, duplication_region_window_start + opt$duplication_region_size, "duplicated_region"), c(chromosome_name, length(neo_sequence$sequence) - opt$duplication_region_size, length(neo_sequence$sequence), "duplicata_region"))
-      
-      non_homologuous.info <- do.call(rbind, nohomoseq.partition)
-      non_homologuous.info <- apply(non_homologuous.info[, c("chrom", "start", "stop", "class")], 2, as.character)
-      #apply((t(sapply(nohomoseq.partition, function(x) c(x$chrom, x$start - opt$slope, x$start + opt$slope, x$class)))), 2, as.character)
-      
+      transposons.info <- as.data.frame(matrix(unlist(transposons.info), ncol = 5, byrow = T))
+
+      deletions.info <- sapply(deletions.partition, function(x){
+        lapply(opt$slope, function(one_slope){
+          list(c(chromosome_name, x$start - one_slope, x$start, "DR_scar_regions", one_slope), c(chromosome_name, x$stop, x$stop + one_slope, "DR_scar_regions", one_slope))
+        })
+      })
+      deletions.info <- as.data.frame(matrix(unlist(deletions.info), ncol = 5, byrow = T))
+
+      duplication.info <- rbind(c(chromosome_name, duplication_region_window_start, duplication_region_window_start + opt$duplication_region_size, "duplicated_region", NA))
+
+      non_homologuous.info <- do.call(rbind, sapply(nohomoseq.partition, function(x){
+        lapply(opt$slope, function(one_slope){
+          list(c(chromosome_name, x$start - one_slope, x$start + one_slope, "insertion_flanking_regions", one_slope))
+        })
+      }))
+      non_homologuous.info <- as.data.frame(matrix(unlist(non_homologuous.info), ncol = 5, byrow = T))
+
     } #ANNOTATING THE NEW GENOME
     
     nonhomologsequences.df <- as.data.frame(t(do.call(cbind, nohomoseq.partition)))
     nonhomologsequences.df <- nonhomologsequences.df[, !names(nonhomologsequences.df) %in% c("sequence")]
-    
+
     bed_annotation <- apply(rbind(transposons.info, deletions.info, duplication.info, non_homologuous.info), 2, as.character)
     results <- bind_rows(as.data.frame(t(do.call(cbind, deletions.partition))), as.data.frame(t(do.call(cbind, transposon_jumps.partition))), nonhomologsequences.df)
     
@@ -308,7 +312,7 @@ for(SV_set in 1:opt$structural_variants){
   
   write.fasta(neo_sequence$sequence, file.out = paste0(SV_folder_path, "/", opt$prefix, "_unmuted.fasta"), names = chromosome_name)
   ref <- read_fasta(paste0(SV_folder_path, "/", opt$prefix, "_unmuted.fasta"), cut_names = T)
-  
+
   if(!opt$unmuted) {
     unlink(paste(SV_folder_path, "/", opt$prefix, "_unmuted.fasta", sep = ""))
   }
